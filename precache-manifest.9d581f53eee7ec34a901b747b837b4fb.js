74836bd6615c08af30a1a178bb3618d4f2e7701b
Bu-Qi <1204580150@qq.com>
2021-07-03 19:53:11 +0800

    Updates

---
+++ precache-manifest.9d581f53eee7ec34a901b747b837b4fb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cfee44beb69a31905c4b847a169d15",
+    "url": "/games1/index.html"
+  },
+  {
+    "revision": "8ec0ae6a1b9d3ee000a1",
+    "url": "/games1/static/css/main.3b7fb9e2.chunk.css"
+  },
+  {
+    "revision": "551ced5daa8cd3c747a2",
+    "url": "/games1/static/js/2.a79c146d.chunk.js"
+  },
+  {
+    "revision": "04e17be27363ea674ab11e8fe5174a4f",
+    "url": "/games1/static/js/2.a79c146d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ec0ae6a1b9d3ee000a1",
+    "url": "/games1/static/js/main.a085723c.chunk.js"
+  },
+  {
+    "revision": "593b12968a093f97d3b3",
+    "url": "/games1/static/js/runtime-main.21b20f84.js"
+  },
+  {
+    "revision": "d2b2559b651e6a50f8d43f206dcdb8e7",
+    "url": "/games1/static/media/Btn_Register_Normal.d2b2559b.png"
+  },
+  {
+    "revision": "d3684ace4f10c62c9209b45d3bb724c2",
+    "url": "/games1/static/media/Btn_Register_Over.d3684ace.png"
+  },
+  {
+    "revision": "64a6f16c4f5ef7e189e75dfc0a52a25f",
+    "url": "/games1/static/media/homecard@2x.64a6f16c.png"
+  },
+  {
+    "revision": "9d0437bbb5e705bad95ac929361faf3e",
+    "url": "/games1/static/media/index_bg.9d0437bb.png"
+  },
+  {
+    "revision": "41236031fc933b053717ef1760601e36",
+    "url": "/games1/static/media/start_btn.41236031.png"
+  },
+  {
+    "revision": "2a4a2235f6ed0e838b49a3f1bec4aae1",
+    "url": "/games1/static/media/tester-bg@2x.2a4a2235.png"
+  },
+  {
+    "revision": "b102b8003aab95865cb7363428273a16",
+    "url": "/games1/static/media/walls.b102b800.jpg"
+  },
+  {
+    "revision": "08087132d6dcfcd5c5f471880c104712",
+    "url": "/games1/static/media/zombierun.08087132.png"
+  }
+]);
